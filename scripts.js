9fb29e0262d2a5696af9d164349d935d66a32b21
Patryk Eleganczyk <patos87@o2.pl>
2020-08-22 17:20:48 +0100

    capturing values in js added

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let heightElement = document.querySelector(".js-height");
+let weightElement = document.querySelector(".js-weight");
+let formElement = document.querySelector(".js-form");
+let bmiElement = document.querySelector(".js-bmi");
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let height = heightElement.value;
+    let wight = weightElement.value;
+}); 
+
+
